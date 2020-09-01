6a1db02363183cb2b2919cdca997eaae1d8f5aea
s3757937 <s3757937@rmit.edu.vn>
2020-09-01 11:03:59 +0700

    1/9/20 1st

--- src/Tank/Tank.java
+++ src/Tank/Tank.java
@@ -5,6 +5,9 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
@@ -33,7 +36,7 @@ class Hull {
             case 4:
                 colorPath = "D";
         }
-        return "src/PNG/Hulls_Color_" + colorPath + "/Hull_0" + type + ".png";
+        return "file:"+"src/PNG/Hulls_Color_" + colorPath + "/Hull_0" + type + ".png";
     }
 }
 
@@ -58,7 +61,7 @@ class Weapon {
             case 4:
                 colorPath = "D";
         }
-        return "src/PNG/Weapon_Color_" + colorPath + "/Gun_0" + Option + ".png";
+        return "file:"+"src/PNG/Weapon_Color_" + colorPath + "/Gun_0" + Option + ".png";
     }
 }
 
@@ -67,15 +70,16 @@ class Track {
     private int Option;
 
     Track(int Option) {
+        Option=Option%3;
         this.Option = Option;
     }
 
     public String getTrackPath(int TrackPathOption) {
-        return "src/PNG/Effects/Tire_Track_0" + TrackPathOption + ".png";
+        return "file:"+"src/PNG/Effects/Tire_Track_0" + TrackPathOption + ".png";
     }
 
     public String getTrack(String Animation) {
-        return "src/PNG/Tracks/Track_" + Option + "_" + Animation + ".png";
+        return "file:"+"src/PNG/Tracks/Track_" + Option + "_" + Animation + ".png";
     }
 }
 
@@ -128,7 +132,9 @@ class Bullet {
 }
 
 public class Tank extends Application {
-    public Tank(){
+
+    Group tank=new Group(createTank(1,1,10));
+    public Tank() throws FileNotFoundException {
     }
 
     @Override
@@ -137,15 +143,32 @@ public class Tank extends Application {
         stage.setTitle("Loading an image");
 
         //Adding scene to the stage
-        stage.setScene(createTank(1,1));
+        GridPane tankkk =new GridPane();
+        tankkk.getChildren().addAll(createTank(1,1,10));
+        tankkk.setOnKeyPressed(e -> {
+            switch (e.getCode()) {
+                case DOWN: tankkk.setLayoutY(tankkk.getLayoutY()+10);
+                    System.out.println("DAMN");
 
+                case UP: tankkk.setLayoutY(tankkk.getLayoutY() - 10);
+                case LEFT: tankkk.setLayoutX(tankkk.getLayoutX() - 10);
+                case RIGHT: tankkk.setLayoutX(tankkk.getLayoutX() + 10);
+            }
+        });
+
+        tankkk.setRotate(180);
+//        tankkk.setLayoutX(200);
+//        tankkk.setLayoutY(tankkk.getLayoutY()+100);
         //Displaying the contents of the stage
+
+        Scene scene = new Scene(tankkk,1000, 800);
+        stage.setScene(scene);
         stage.show();
 
 
     }
 
-    public Scene createTank(int choice, int color) throws FileNotFoundException {
+    public Group createTank(int choice, int color,int x) throws FileNotFoundException {
         Hull hull= new Hull(color,choice);
         Bullet bullet= new Bullet(choice);
         Weapon weapon= new Weapon(color,choice);
@@ -157,14 +180,55 @@ public class Tank extends Application {
         Image trackI_B= new Image(track.getTrack("B"));
 
         ImageView TankView= new ImageView(HullI);
-        TankView.setX(50);
-        TankView.setY(25);
-        TankView.setFitHeight(455);
-        TankView.setFitWidth(500);
-        Group root = new Group(TankView);
-        //Creating a scene object
-        Scene scene = new Scene(root, 600, 500);
-        return scene;
-    }
+        ImageView TrackViewA1= new ImageView(trackI_A);
+        ImageView TrackViewA2= new ImageView(trackI_A);
+        ImageView TrackViewA3= new ImageView(trackI_A);
+        ImageView TrackViewA4= new ImageView(trackI_A);
+        ImageView WeaponView= new ImageView(weaponI);
+        ImageView TrackviewB= new ImageView(trackI_B);
+        TrackViewA1.setX(2.2*x);
+        TrackViewA1.setY(0.2*x);
+        TrackViewA1.setFitHeight(4*x);
+        TrackViewA1.setFitWidth(1.3*x);
+        TrackViewA2.setX(2.1*x);
+        TrackViewA2.setY(5*x);
+        TrackViewA2.setFitHeight(4*x);
+        TrackViewA2.setFitWidth(1.5*x);
 
+        TrackViewA3.setX(5.5*x);
+        TrackViewA3.setY(0.2*x);
+        TrackViewA3.setFitHeight(4*x);
+        TrackViewA3.setFitWidth(1.3*x);
+        TrackViewA4.setX(5.5*x);
+        TrackViewA4.setY(5*x);
+        TrackViewA4.setFitHeight(4*x);
+        TrackViewA4.setFitWidth(1.5*x);
+
+        TrackviewB.setX(0);
+        TrackviewB.setFitHeight(9*x);
+        TrackviewB.setFitWidth(7*x);
+
+        TankView.setX(1.5*x);
+        TankView.setY(0.5*x);
+        TankView.setFitHeight(8*x);
+        TankView.setFitWidth(6*x);
+
+        WeaponView.setX(3*x);
+        WeaponView.setFitHeight(7*x);
+        WeaponView.setFitWidth(3*x);
+
+        Group root = new Group(TrackViewA1,TrackViewA2,TrackViewA3,TrackViewA4,TankView,WeaponView);
+        //Creating a scene object
+        return root;
+    }
+    public void Move(GridPane tankkk){
+        tankkk.setOnKeyPressed(e -> {
+            switch (e.getCode()) {
+                case DOWN: tankkk.setLayoutY(tankkk.getLayoutY()+10); break;
+                case UP: tankkk.setLayoutY(tankkk.getLayoutY() - 10); break;
+                case LEFT: tankkk.setLayoutX(tankkk.getLayoutX() - 10); break;
+                case RIGHT: tankkk.setLayoutX(tankkk.getLayoutX() + 10); break;
+            }
+        });
+    }
 }
