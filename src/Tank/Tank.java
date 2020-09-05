39fdb2258938d3a40285cb9b99c4bae1e74e9721
mystery <s3777242@rmit.edu.vn>
2020-09-05 15:02:41 +0700

    OK

--- src/Tank/Tank.java
+++ src/Tank/Tank.java
@@ -387,12 +387,20 @@ public class Tank extends Application {
     }
 
     public void loadMap() {
-        // Load image
+
+        // Background image
         Image backgroundImage1 = new Image("file:src/Ultilities/PNG/Ground_Tile_01_C.png");
         Image backgroundImage2 = new Image("file:src/Ultilities/PNG/Ground_Tile_02_C.png");
-        Image backgroundImage3 = new Image("file:src/Ultilities/PNG/Water_Tile.jpg");
-        Image backgroundImage4 = new Image("file:src/Ultilities/PNG/Sand.png");
-        Image backgroundImage5 = new Image("file:src/Ultilities/PNG/Sand2.png");
+        Image backgroundImage3 = new Image("file:src/Ultilities/PNG/NguyenCustomBuild/Water_Tile.jpg");
+        Image backgroundImage4 = new Image("file:src/Ultilities/PNG/NguyenCustomBuild/Sand.png");
+        Image backgroundImage5 = new Image("file:src/Ultilities/PNG/NguyenCustomBuild/Sand2.png");
+
+        // Border image
+        Image borderImage1 = new Image("file:src/Ultilities/PNG/NguyenCustomBuild/TopLeftBorder.png");
+        Image borderImage2 = new Image("file:src/Ultilities/PNG/NguyenCustomBuild/TopRightBorder.png");
+        Image borderImage3 = new Image("file:src/Ultilities/PNG/NguyenCustomBuild/BottomLeftBorder.png");
+        Image borderImage4 = new Image("file:src/Ultilities/PNG/NguyenCustomBuild/BottomRightBorder.png");
+
 
         //Pipe
         Image objectPipe = new Image("file:src/Ultilities/PNG/Decor_Items/Decor_Pipe_B.png");
@@ -406,44 +414,76 @@ public class Tank extends Application {
         ImageView[] imageView = new ImageView[20];
         for (int i = 0; i < 20; i++) {
             imageView[i] = new ImageView();
-            imageView[i].setImage(backgroundImage4);
-            imageView[i].setFitHeight(70);
-            imageView[i].setFitWidth(70);
-            imageView[i].setTranslateX(70 * i);
-            imageView[i].setTranslateY(700);
+            if (i % 2 == 0) {
+                imageView[i].setImage(backgroundImage1);
+                imageView[i].setFitHeight(70);
+                imageView[i].setFitWidth(70);
+                imageView[i].setTranslateX(70 * i);
+                imageView[i].setTranslateY(700);
+            } else {
+                imageView[i].setImage(backgroundImage2);
+                imageView[i].setFitHeight(70);
+                imageView[i].setFitWidth(70);
+                imageView[i].setTranslateX(70 * i);
+                imageView[i].setTranslateY(700);
+            }
         }
 
         //10 row upwards
         ImageView[] imageView2 = new ImageView[20];
         for (int i = 0; i < 20; i++) {
             imageView2[i] = new ImageView();
-            imageView2[i].setImage(backgroundImage4);
-            imageView2[i].setFitHeight(70);
-            imageView2[i].setFitWidth(70);
-            imageView2[i].setTranslateX(70 * i);
-            imageView2[i].setTranslateY(630);
+            if (i % 2 != 0) {
+                imageView2[i].setImage(backgroundImage1);
+                imageView2[i].setFitHeight(70);
+                imageView2[i].setFitWidth(70);
+                imageView2[i].setTranslateX(70 * i);
+                imageView2[i].setTranslateY(630);
+            } else {
+                imageView2[i].setImage(backgroundImage2);
+                imageView2[i].setFitHeight(70);
+                imageView2[i].setFitWidth(70);
+                imageView2[i].setTranslateX(70 * i);
+                imageView2[i].setTranslateY(630);
+            }
         }
 
         // y = 560
         ImageView[] imageView3 = new ImageView[20];
         for (int i = 0; i < 20; i++) {
             imageView3[i] = new ImageView();
-            imageView3[i].setImage(backgroundImage4);
-            imageView3[i].setFitHeight(70);
-            imageView3[i].setFitWidth(70);
-            imageView3[i].setTranslateX(70 * i);
-            imageView3[i].setTranslateY(560);
+            if (i % 2 != 0) {
+                imageView3[i].setImage(backgroundImage2);
+                imageView3[i].setFitHeight(70);
+                imageView3[i].setFitWidth(70);
+                imageView3[i].setTranslateX(70 * i);
+                imageView3[i].setTranslateY(560);
+            } else {
+                imageView3[i].setImage(backgroundImage1);
+                imageView3[i].setFitHeight(70);
+                imageView3[i].setFitWidth(70);
+                imageView3[i].setTranslateX(70 * i);
+                imageView3[i].setTranslateY(560);
+            }
         }
 
         // y = 490
         ImageView[] imageView4 = new ImageView[20];
         for (int i = 0; i < 20; i++) {
             imageView4[i] = new ImageView();
-            imageView4[i].setImage(backgroundImage4);
-            imageView4[i].setFitHeight(70);
-            imageView4[i].setFitWidth(70);
-            imageView4[i].setTranslateX(70 * i);
-            imageView4[i].setTranslateY(490);
+            if (i % 2 == 0) {
+                imageView4[i].setImage(backgroundImage2);
+                imageView4[i].setFitHeight(70);
+                imageView4[i].setFitWidth(70);
+                imageView4[i].setTranslateX(70 * i);
+                imageView4[i].setTranslateY(490);
+            } else {
+                imageView4[i].setImage(backgroundImage1);
+                imageView4[i].setFitHeight(70);
+                imageView4[i].setFitWidth(70);
+                imageView4[i].setTranslateX(70 * i);
+                imageView4[i].setTranslateY(490);
+            }
 
         }
 
@@ -451,77 +491,218 @@ public class Tank extends Application {
         ImageView[] imageView5 = new ImageView[20];
         for (int i = 0; i < 20; i++) {
             imageView5[i] = new ImageView();
-            imageView5[i].setImage(backgroundImage4);
-            imageView5[i].setFitHeight(70);
-            imageView5[i].setFitWidth(70);
-            imageView5[i].setTranslateX(70 * i);
-            imageView5[i].setTranslateY(420);
+            if (i % 2 != 0) {
+                imageView5[i].setImage(backgroundImage2);
+                imageView5[i].setFitHeight(70);
+                imageView5[i].setFitWidth(70);
+                imageView5[i].setTranslateX(70 * i);
+                imageView5[i].setTranslateY(420);
+            } else {
+                imageView5[i].setImage(backgroundImage1);
+                imageView5[i].setFitHeight(70);
+                imageView5[i].setFitWidth(70);
+                imageView5[i].setTranslateX(70 * i);
+                imageView5[i].setTranslateY(420);
+            }
         }
 
         // y = 350, midpoint
         ImageView[] imageView6 = new ImageView[20];
         for (int i = 0; i < 20; i++) {
             imageView6[i] = new ImageView();
-            imageView6[i].setImage(backgroundImage3);
-            imageView6[i].setFitHeight(70);
-            imageView6[i].setFitWidth(70);
-            imageView6[i].setTranslateX(70 * i);
-            imageView6[i].setTranslateY(350);
+            if (i % 2 == 0) {
+                imageView6[i].setImage(backgroundImage2);
+                imageView6[i].setFitHeight(70);
+                imageView6[i].setFitWidth(70);
+                imageView6[i].setTranslateX(70 * i);
+                imageView6[i].setTranslateY(350);
+            } else {
+                imageView6[i].setImage(backgroundImage1);
+                imageView6[i].setFitHeight(70);
+                imageView6[i].setFitWidth(70);
+                imageView6[i].setTranslateX(70 * i);
+                imageView6[i].setTranslateY(350);
+            }
         }
 
         // y = 280
         ImageView[] imageView7 = new ImageView[20];
         for (int i = 0; i < 20; i++) {
             imageView7[i] = new ImageView();
-            imageView7[i].setImage(backgroundImage5);
-            imageView7[i].setFitHeight(70);
-            imageView7[i].setFitWidth(70);
-            imageView7[i].setTranslateX(70 * i);
-            imageView7[i].setTranslateY(280);
+            if (i % 2 != 0) {
+                imageView7[i].setImage(backgroundImage2);
+                imageView7[i].setFitHeight(70);
+                imageView7[i].setFitWidth(70);
+                imageView7[i].setTranslateX(70 * i);
+                imageView7[i].setTranslateY(280);
+            }else {
+                imageView7[i].setImage(backgroundImage1);
+                imageView7[i].setFitHeight(70);
+                imageView7[i].setFitWidth(70);
+                imageView7[i].setTranslateX(70 * i);
+                imageView7[i].setTranslateY(280);
+            }
         }
 
         // y = 210
         ImageView[] imageView8 = new ImageView[20];
         for (int i = 0; i < 20; i++) {
             imageView8[i] = new ImageView();
-            imageView8[i].setImage(backgroundImage5);
-            imageView8[i].setFitHeight(70);
-            imageView8[i].setFitWidth(70);
-            imageView8[i].setTranslateX(70 * i);
-            imageView8[i].setTranslateY(210);
+            if (i % 2 == 0) {
+                imageView8[i].setImage(backgroundImage2);
+                imageView8[i].setFitHeight(70);
+                imageView8[i].setFitWidth(70);
+                imageView8[i].setTranslateX(70 * i);
+                imageView8[i].setTranslateY(210);
+            } else {
+                imageView8[i].setImage(backgroundImage1);
+                imageView8[i].setFitHeight(70);
+                imageView8[i].setFitWidth(70);
+                imageView8[i].setTranslateX(70 * i);
+                imageView8[i].setTranslateY(210);
+            }
         }
 
         // y = 140
         ImageView[] imageView9 = new ImageView[20];
         for (int i = 0; i < 20; i++) {
             imageView9[i] = new ImageView();
-            imageView9[i].setImage(backgroundImage5);
-            imageView9[i].setFitHeight(70);
-            imageView9[i].setFitWidth(70);
-            imageView9[i].setTranslateX(70 * i);
-            imageView9[i].setTranslateY(140);
+            if (i % 2 != 0) {
+                imageView9[i].setImage(backgroundImage2);
+                imageView9[i].setFitHeight(70);
+                imageView9[i].setFitWidth(70);
+                imageView9[i].setTranslateX(70 * i);
+                imageView9[i].setTranslateY(140);
+            } else {
+                imageView9[i].setImage(backgroundImage2);
+                imageView9[i].setFitHeight(70);
+                imageView9[i].setFitWidth(70);
+                imageView9[i].setTranslateX(70 * i);
+                imageView9[i].setTranslateY(140);
+            }
         }
 
         // y = 70
         ImageView[] imageView10 = new ImageView[20];
         for (int i = 0; i < 20; i++) {
-            imageView10[i] = new ImageView();
-            imageView10[i].setImage(backgroundImage5);
-            imageView10[i].setFitHeight(70);
-            imageView10[i].setFitWidth(70);
-            imageView10[i].setTranslateX(70 * i);
-            imageView10[i].setTranslateY(70);
+            if (i % 2 == 0) {
+                imageView10[i] = new ImageView();
+                imageView10[i].setImage(backgroundImage2);
+                imageView10[i].setFitHeight(70);
+                imageView10[i].setFitWidth(70);
+                imageView10[i].setTranslateX(70 * i);
+                imageView10[i].setTranslateY(70);
+            } else {
+                imageView10[i] = new ImageView();
+                imageView10[i].setImage(backgroundImage1);
+                imageView10[i].setFitHeight(70);
+                imageView10[i].setFitWidth(70);
+                imageView10[i].setTranslateX(70 * i);
+                imageView10[i].setTranslateY(70);
+            }
         }
 
         // Top position
         ImageView[] imageView11 = new ImageView[20];
         for (int i = 0; i < 20; i++) {
-            imageView11[i] = new ImageView();
-            imageView11[i].setImage(backgroundImage5);
-            imageView11[i].setFitHeight(70);
-            imageView11[i].setFitWidth(70);
-            imageView11[i].setTranslateX(70 * i);
-            imageView11[i].setTranslateY(0);
+            if (i % 2 != 0) {
+                imageView11[i] = new ImageView();
+                imageView11[i].setImage(backgroundImage2);
+                imageView11[i].setFitHeight(70);
+                imageView11[i].setFitWidth(70);
+                imageView11[i].setTranslateX(70 * i);
+                imageView11[i].setTranslateY(0);
+            } else {
+                imageView11[i] = new ImageView();
+                imageView11[i].setImage(backgroundImage1);
+                imageView11[i].setFitHeight(70);
+                imageView11[i].setFitWidth(70);
+                imageView11[i].setTranslateX(70 * i);
+                imageView11[i].setTranslateY(0);
+            }
+        }
+
+        // Map border
+        ImageView[] borderViewBottom = new ImageView[20];
+        for (int i = 0; i < 20; i++) {
+            borderViewBottom[i] = new ImageView();
+            if (i == 0) {
+                borderViewBottom[i].setImage(borderImage3);
+                borderViewBottom[i].setFitHeight(70);
+                borderViewBottom[i].setFitWidth(350);
+                borderViewBottom[i].setScaleX(1);
+                borderViewBottom[i].setScaleY(1);
+                borderViewBottom[i].setTranslateX(70 * i);
+                borderViewBottom[i].setTranslateY(630);
+            }
+            if (i == 5) {
+                borderViewBottom[i].setImage(borderImage4);
+                borderViewBottom[i].setFitHeight(70);
+                borderViewBottom[i].setFitWidth(350);
+                borderViewBottom[i].setScaleX(1);
+                borderViewBottom[i].setScaleY(1);
+                borderViewBottom[i].setTranslateX(70 * i);
+                borderViewBottom[i].setTranslateY(630);
+            }
+            if (i == 10) {
+                borderViewBottom[i].setImage(borderImage3);
+                borderViewBottom[i].setFitHeight(70);
+                borderViewBottom[i].setFitWidth(350);
+                borderViewBottom[i].setScaleX(1);
+                borderViewBottom[i].setScaleY(1);
+                borderViewBottom[i].setTranslateX(70 * i);
+                borderViewBottom[i].setTranslateY(630);
+            }
+            if (i == 15) {
+                borderViewBottom[i].setImage(borderImage4);
+                borderViewBottom[i].setFitHeight(70);
+                borderViewBottom[i].setFitWidth(350);
+                borderViewBottom[i].setScaleX(1);
+                borderViewBottom[i].setScaleY(1);
+                borderViewBottom[i].setTranslateX(70 * i);
+                borderViewBottom[i].setTranslateY(630);
+            }
+        }
+
+        ImageView[] borderViewTop = new ImageView[20];
+        for (int i = 0; i < 20; i++) {
+            borderViewTop[i] = new ImageView();
+            if (i == 0) {
+                borderViewTop[i].setImage(borderImage1);
+                borderViewTop[i].setFitHeight(70);
+                borderViewTop[i].setFitWidth(350);
+                borderViewTop[i].setScaleX(1);
+                borderViewTop[i].setScaleY(1);
+                borderViewTop[i].setTranslateX(70 * i);
+                borderViewTop[i].setTranslateY(0);
+            }
+            if (i == 5) {
+                borderViewTop[i].setImage(borderImage2);
+                borderViewTop[i].setFitHeight(70);
+                borderViewTop[i].setFitWidth(350);
+                borderViewTop[i].setScaleX(1);
+                borderViewTop[i].setScaleY(1);
+                borderViewTop[i].setTranslateX(70 * i);
+                borderViewTop[i].setTranslateY(0);
+            }
+            if (i == 10) {
+                borderViewTop[i].setImage(borderImage1);
+                borderViewTop[i].setFitHeight(70);
+                borderViewTop[i].setFitWidth(350);
+                borderViewTop[i].setScaleX(1);
+                borderViewTop[i].setScaleY(1);
+                borderViewTop[i].setTranslateX(70 * i);
+                borderViewTop[i].setTranslateY(0);
+            }
+            if (i == 15) {
+                borderViewTop[i].setImage(borderImage2);
+                borderViewTop[i].setFitHeight(70);
+                borderViewTop[i].setFitWidth(350);
+                borderViewTop[i].setScaleX(1);
+                borderViewTop[i].setScaleY(1);
+                borderViewTop[i].setTranslateX(70 * i);
+                borderViewTop[i].setTranslateY(0);
+            }
         }
 
         // Add object to the map
@@ -600,7 +781,8 @@ public class Tank extends Application {
         tankkk.getChildren().addAll(imageView10);
         tankkk.getChildren().addAll(imageView11);
 
-        // Add objects to the map
+        tankkk.getChildren().addAll(borderViewTop);
+         //Add objects to the map
         tankkk.getChildren().addAll(objectView);
         tankkk.getChildren().addAll(objectView2);
     }
