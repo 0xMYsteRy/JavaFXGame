0aedc43b48715f858f1cb52ec4ed93836955aeae
Nguyen <s3777242@rmit.edu.vn>
2020-09-06 17:52:44 +0700

    New map

--- src/Map_JAVA/Map.java
+++ src/Map_JAVA/Map.java
@@ -1,6 +1,5 @@
 package Map_JAVA;
 
-import Tank_JAVA.Tank;
 import javafx.application.Application;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -8,121 +7,85 @@ import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Map extends Application {
-    Image backgroundImage1;
-    Image backgroundImage2;
-    Image objectPipe;
-    Image objectPipeHeadLeft;
-    Image objectPipeHeadRight;
-    Image[] img;
-    ImageView[] imageView;
-
     // Constructor
     public Map() {
     }
 
     @Override
     public void start(Stage stage) throws Exception {
-        //tankPane = new Pane();
     }
 
     public void loadMap(Pane tankPane) {
-        backgroundImage1 = new Image("file:src/Ultilities/PNG/Ground_Tile_01_C.png");
-        // Load image
-        backgroundImage2 = new Image("file:src/Ultilities/PNG/Ground_Tile_02_C.png");
 
-        //Pipe
-        objectPipe = new Image("file:src/Ultilities/PNG/Decor_Items/Decor_Pipe_B.png");
-        objectPipeHeadLeft = new Image("file: src/Ultilities/PNG/Decor_Items/Decor_Pipe_A_02.png");
-        objectPipeHeadRight = new Image("file:src/Ultilities/PNG/Decor_Items/Decor_Pipe_C_01.png");
-
-        //Item
-        Image objectTree = new Image("file:src/GuttyKreumNatureTilesvol1/tree96x96transparent.png");
-
-        img = new Image[]{backgroundImage1, backgroundImage2};
-
+        //Draw background
         int[][] pvpMap = {
-                {1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2},
-                {2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1},
-                {1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2},
-                {2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1},
-                {1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2},
-                {2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1},
-                {1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2},
-                {2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1},
-                {1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2},
-                {2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1},
-                {1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2},
+                {2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2},
+                {2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2},
+                {2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2},
         };
-        int[][] objectMap = {
-                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-                {1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-        };
-
-
-//        ImageView[] imageView11 = new ImageView[20];
-//        for (int i = 0; i < 20; i++) {
-//            imageView11[i] = new ImageView();
-//            imageView11[i].setImage(objectTree);
-//            if (i == 0) {
-////                imageView11[i].setScaleX(0);
-////                imageView11[i].setScaleY(0);
-//                imageView11[i].setFitHeight(70);
-//                imageView11[i].setFitWidth(70);
-//                imageView11[i].setTranslateX(70 * i);
-//                imageView11[i].setTranslateY(540);
-//            }
-//        }
-
         drawMap(tankPane, pvpMap);
+
+        //Draw object
+        int[][] objectMap = {
+                {4, 1, 3, 1, 1, 5, 5, 5, 5, 1, 1, 1, 1, 1, 1, 1, 1, 3, 1, 4},
+                {1, 1, 3, 1, 1, 1, 5, 5, 1, 1, 1, 1, 1, 1, 2, 1, 1, 3, 1, 1},
+                {3, 3, 3, 1, 1, 6, 1, 5, 1, 1, 1, 1, 1, 1, 6, 1, 1, 3, 3, 3},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 5, 7, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 7, 5, 1},
+                {7, 5, 5, 5, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 5, 5, 5, 7},
+                {1, 5, 5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 5, 5, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {3, 3, 3, 1, 1, 6, 1, 1, 1, 1, 1, 1, 1, 5, 6, 1, 1, 3, 3, 3},
+                {1, 1, 3, 1, 1, 2, 1, 1, 1, 1, 1, 1, 5, 5, 1, 1, 1, 3, 1, 1},
+                {4, 1, 3, 1, 1, 1, 1, 1, 1, 1, 1, 5, 5, 5, 5, 1, 1, 3, 1, 4},
+        };
         drawObject(tankPane, objectMap);
-        //tankPane.getChildren().addAll(imageView11);
 
-    }
+        // Draw Circle
+        int[][] circleMap = {
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        };
+        //drawCircleObject(tankPane, circleMap);
 
-    public void drawObject(Pane pane, int[][] objectMap) {
-        for (int i = 0; i < 11; i++) {
-            for (int j = 0; j < 20; j++) {
-                // System.out.println(i+" "+j);
-                String imagePath = callImageObject(objectMap[i][j]);
-                ImageView groundCurrent = new ImageView();
-                groundCurrent.setImage(new Image(imagePath));
-                groundCurrent.setFitHeight(70);
-                groundCurrent.setFitWidth(70);
-                groundCurrent.setTranslateX(70 * j);
-                groundCurrent.setTranslateY(70 * i);
-                pane.getChildren().addAll(groundCurrent);
-            }
-        }
-    }
-
-    public String callImageObject(int choice) {
-        switch (choice) {
-            case 1:
-                return "file:src/Ultilities/PNG/Ground_Tile_01_C.png";
-            case 2:
-                return "file:src/GuttyKreumNatureTilesvol1/tree96x96transparent.png";
-            case 3:
-                return "file:src/Ultilities/PNG/Ground_Tile_02_C.png";
-            default:
-                System.out.println("Wrong image Choice!");
-                return "Wrong choice";
-        }
+        // Draw castle
+        int[][] castleMap = {
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        };
+        drawCastleObject(tankPane, castleMap);
     }
 
     public void drawMap(Pane pane, int[][] map) {
         for (int i = 0; i < 11; i++) {
             for (int j = 0; j < 20; j++) {
-                // System.out.println(i+" "+j);
-                String imagePath = callImage(map[i][j]);
+                String imagePath = getImagePath(map[i][j]);
                 ImageView groundCurrent = new ImageView();
                 groundCurrent.setImage(new Image(imagePath));
                 groundCurrent.setFitHeight(70);
@@ -134,19 +97,113 @@ public class Map extends Application {
         }
     }
 
-    public String callImage(int choice) {
+    public String getImagePath(int choice) {
         switch (choice) {
             case 1:
-                return "file:src/Ultilities/PNG/Ground_Tile_01_C.png";
+                return "file:src/Ultilities/PNG/Background_Snow.png";
             case 2:
-                return "file:src/Ultilities/PNG/Ground_Tile_02_C.png";
-            case 3:
-                return "file:src/Ultilities/PNG/Ground_Tile_02_C.png";
+                return "file:src/Ultilities/PNG/Background_DarkSnow.png";
+            case 3 :
+                return "file:src/Ultilities/PNG/bigcircle.png";
             default:
-                System.out.println("Wrong image Choice!");
-                return "Wrong choice";
+                return "";
         }
     }
 
+    public String getObjectImagePath(int choice) {
+        switch (choice) {
+            case 1:
+                //return nothing if the no need to add object on that tile.
+                return "file:";
+            case 2:
+                return "file:src/Ultilities/PNG/Puddle.png";
+            case 3:
+                return "file:src/Ultilities/PNG/HouseObject.png";
+            case 4:
+                return "file:src/Ultilities/PNG/ObjectBase.png";
+            case 5:
+                return "file:src/Ultilities/PNG/ObjectTree.png";
+            case 6:
+                return "file:src/Ultilities/PNG/bighouse.png";
+            case 7:
+                return "file:src/Ultilities/PNG/Tower.png";
+            default:
+                return "";
+        }
+    }
 
+    public void drawObject(Pane pane, int[][] map) {
+        for (int i = 0; i < 11; i++) {
+            for (int j = 0; j < 20; j++) {
+                String imagePath = getObjectImagePath(map[i][j]);
+                ImageView objectTile = new ImageView();
+                objectTile.setImage(new Image(imagePath));
+                objectTile.setFitHeight(70);
+                objectTile.setFitWidth(70);
+                objectTile.setTranslateX(70 * j);
+                objectTile.setTranslateY(70 * i);
+                pane.getChildren().addAll(objectTile);
+            }
+        }
+    }
+
+    //Draw Castle
+    public String getCastleImagePath(int choice) {
+        switch (choice) {
+            case 1:
+                //return nothing if the no need to add object on that tile.
+                return "file:";
+            case 2:
+                return "file:src/Ultilities/PNG/Background_Castle.png";
+            default:
+                return "file:src/Ultilities/PNG/ObjectBase.png";
+        }
+    }
+
+    public void drawCastleObject(Pane pane, int[][] map) {
+        for (int i = 0; i < 11; i++) {
+            for (int j = 0; j < 20; j++) {
+                String imagePath = getCastleImagePath(map[i][j]);
+                ImageView CastleTile = new ImageView();
+                CastleTile.setImage(new Image(imagePath));
+                CastleTile.setScaleX(1.5);
+                CastleTile.setScaleY(1.5);
+                CastleTile.setFitHeight(140);
+                CastleTile.setFitWidth(140);
+                CastleTile.setTranslateX(70 * j - 70);
+                CastleTile.setTranslateY(70 * i - 70);
+                pane.getChildren().addAll(CastleTile);
+            }
+        }
+    }
+
+    // Draw circle
+    public String getCircleImagePath(int choice) {
+        switch (choice) {
+            case 1:
+                //return nothing if the no need to add object on that tile.
+                return "file:";
+            case 2:
+                return "file:src/Ultilities/PNG/bigcircle.png";
+            default:
+                return "file:";
+        }
+    }
+
+    public void drawCircleObject(Pane pane, int[][] map) {
+        for (int i = 0; i < 11; i++) {
+            for (int j = 0; j < 20; j++) {
+                String imagePath = getCircleImagePath(map[i][j]);
+                ImageView CircleTile = new ImageView();
+                CircleTile.setImage(new Image(imagePath));
+                CircleTile.setScaleX(1.5);
+                CircleTile.setScaleY(1.5);
+                CircleTile.setFitHeight(300);
+                CircleTile.setFitWidth(300);
+                CircleTile.setTranslateX(70 * j - 70);
+                CircleTile.setTranslateY(70 * i - 70);
+                pane.getChildren().addAll(CircleTile);
+            }
+        }
+    }
 }
