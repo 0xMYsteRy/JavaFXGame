5c5811e707c3e775561e43d061a75bcd8778f474
s3757937 <s3757937@rmit.edu.vn>
2020-09-20 17:53:40 +0700

    Fixed Language

--- src/Menu_JAVA/Setting.java
+++ src/Menu_JAVA/Setting.java
@@ -1,11 +1,17 @@
 package Menu_JAVA;
 
 import javafx.application.Application;
+import javafx.beans.property.StringProperty;
+import javafx.beans.property.StringPropertyBase;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
@@ -14,18 +20,25 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.FileNotFoundException;
+import java.util.Locale;
 
-public class Setting extends Application {
+public class Setting {
     Pane root = new Pane();
     MainMenu mainMenu;
+    private Integer numSwitches = 0;
 
     public Setting() throws FileNotFoundException {
 
    }
     public Parent createSettingContent() throws FileNotFoundException {
+        BorderPane content = new BorderPane();
         mainMenu= new MainMenu();
 
-        root.setPrefSize(400, 300);
+        //Load background
+        String imagePath = "file:src/Menu_JAVA/res/background.png";
+        ImageView background = new ImageView(new Image(imagePath));
+
+        root.setPrefSize(1280, 720);
 
         Text settingTitle = new Text("SETTING");
         settingTitle.setTranslateX(1500);
@@ -37,21 +50,29 @@ public class Setting extends Application {
         });
         settingTitle.setFont(Font.loadFont(MainMenu.class.getResource("res/Penumbra-HalfSerif-Std_35114.ttf").toExternalForm(), 30));
 
-        Text language = new Text("Language");
+        Text language = new Text("Menu_JAVA/Language");
         language.setFont(Font.loadFont(MainMenu.class.getResource("res/Penumbra-HalfSerif-Std_35114.ttf").toExternalForm(), 20));
-        Button eng = new Button("English");
-        eng.setMinWidth(100);
-        eng.setMinHeight(20);
-        eng.setOnMouseClicked(e -> {
-        });
-        Button vn = new Button("Tiếng Việt");
-        vn.setMinWidth(100);
-        vn.setMinHeight(20);
-        vn.setOnMouseClicked(e -> {
-        });
-        HBox languageBox = new HBox(20, language, eng, vn);
-        languageBox.setTranslateX(20);
-        languageBox.setTranslateY(120);
+
+        Button buttonEnglish = I18N.buttonForKey("button.english");
+        buttonEnglish.setTooltip(I18N.tooltipForKey("button.english.tooltip"));
+        buttonEnglish.setOnAction((evt) -> switchLanguage(Locale.ENGLISH));
+        buttonEnglish.setMinWidth(100);
+        buttonEnglish.setMinHeight(20);
+
+
+        Button buttonVietNam = I18N.buttonForKey("button.vietnam");
+        buttonVietNam.setTooltip(I18N.tooltipForKey("button.vietnam.tooltip"));
+        buttonVietNam.setOnAction((evt) -> switchLanguage((new Locale("vi", "VN"))));
+        buttonVietNam.setMinWidth(100);
+        buttonVietNam.setMinHeight(20);
+
+        HBox hbox = new HBox();
+        hbox.setPadding(new Insets(5, 5, 5, 5));
+        hbox.setSpacing(5);
+        hbox.getChildren().add(buttonVietNam);
+        hbox.getChildren().add(buttonEnglish);
+        hbox.setTranslateX(20);
+        hbox.setTranslateY(120);
 
         Button control = new Button("Control");
         control.setMinWidth(100);
@@ -80,30 +101,31 @@ public class Setting extends Application {
         closeButton.setTranslateY(270);
         closeButton.setOnMouseClicked(mouseEvent -> {
             try {
-                handleCloseButtonAction(mouseEvent);
+                handleCloseButtonAction();
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
                 System.out.println("Wrong at setting call menu");
             }
         });
 
-        root.getChildren().addAll(settingTitle, languageBox, hbox1, hbox2, closeButton);
+        content.setTop(hbox);
+        final Label label = I18N.labelForValue(() -> I18N.get("label.numSwitches", numSwitches));
+        content.setBottom(label);
+
+        root.getChildren().add(background);
+        //root.getChildren().addAll(settingTitle, hbox);
+        root.getChildren().addAll(settingTitle, hbox, hbox1, hbox2, closeButton);
         return root;
     }
+    private void switchLanguage(Locale locale) {
+        numSwitches++;
+        I18N.setLocale(locale);
+    }
 
-    public void handleCloseButtonAction(MouseEvent event) throws FileNotFoundException {
+    public void handleCloseButtonAction() throws FileNotFoundException {
         primaryStage.getStage().setScene(new Scene(mainMenu.createContent()));
 //        ((Stage)(((Button)event.getSource()).getScene().getWindow())).close();
     }
-
-
-
-    @Override
-    public void start(Stage settingStage) throws Exception {
-        Scene scene = new Scene(createSettingContent());
-        settingStage.setTitle("Setting");
-//        settingStage.setScene(scene);
-        settingStage.show();
-    }
 }
+
 
