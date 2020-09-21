5fd4dc7064d7c250a1a075a0b84bcd5fd9d41dfd
s3757937 <s3757937@rmit.edu.vn>
2020-09-21 20:13:30 +0700

    AAA

--- src/Nguyen_Net/N_Client.java
+++ src/Nguyen_Net/N_Client.java
@@ -31,7 +31,7 @@ public class N_Client extends Application implements Serializable {
     @Override
     public void start(Stage primaryStage) throws Exception  {
         //System.out.println("Not OK");
-        Socket socket = new Socket("127.0.0.1", 80);
+        Socket socket = new Socket("localhost", 80);
         //System.out.println("OK");
         log("Connecting to Server " + socket.getRemoteSocketAddress() + " running on port " + socket.getPort());
         Pane tankPane;
